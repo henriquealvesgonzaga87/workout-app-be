b98f1179c34d1136c9ff16bf41b3369f7ac6dc2e
Henrique Gonzaga <henriquealves87@hotmail.com>
2026-04-27 18:31:16 +0100

    Create training type schema
    Creates the training type schema and migration

--- app/infrastructure/schemas/__init__.py
+++ app/infrastructure/schemas/__init__.py
@@ -1,2 +1,3 @@
 from .base import Base as Base
+from .tarining_type.training_type_schema import TrainingTypeSchema as TrainingTypeSchema
 from .user.user_schema import UserSchema as UserSchema
--- app/settings.py
+++ app/settings.py
@@ -1,7 +1,10 @@
 import os
 from functools import lru_cache
 
+from dotenv import load_dotenv
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+load_dotenv()
 
 
 class Settings(BaseSettings):
